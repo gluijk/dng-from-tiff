dc0cce639868fdd2a98e7c95ef64b9a02a2350ea
Guillermo Luijk <45771557+gluijk@users.noreply.github.com>
2021-04-15 00:20:10 +0200

    Add files via upload

---
+++ dngmaker.bat
@@ -0,0 +1,89 @@
+@echo off
+
+echo ------------------------------------------------------------------------
+echo                         D N G     M A K E R                               
+echo.
+echo   This script creates a valid Bayer DNG file from a Bayer TIFF image
+echo     Step 1: Use exiftool to convert a Bayer TIFF into a DNG
+echo     Step 2: Use dng_validate to make the new DNG valid and compress it
+echo ------------------------------------------------------------------------
+echo.
+
+SETLOCAL EnableDelayedExpansion
+
+rem Clean log and temp files from previous executions
+if exist dngmaker.log del dngmaker.log
+if exist temp.xmp del temp.xmp
+if exist temp.dng del temp.dng
+
+rem Determine number of files and the first file
+set numberOfFiles=0
+set firstFile=
+for %%i in (*.dng) do (
+	set /a numberOfFiles+=1
+	if "!firstFile!"=="" set firstFile=%%~ni
+)
+echo !numberOfFiles! DNG files found, !firstFile!.dng metadata will be used
+
+exiftool -overwrite_original -ExposureTime=0 -ShutterSpeedValue=0 temp.xmp >> dngmaker.log 2>>&1
+
+echo Now it's time to make sure bayer.tif exists
+echo.
+pause
+
+if not exist bayer.tif (
+	echo bayer.tif file doesn't exist >> dngmaker.log
+	goto err
+)
+
+echo Writing clean DNG to !resultDNG!.dng >> dngmaker.log
+
+echo.
+echo Creating DNG based on bayer.tif and the metadata from !firstFile!.dng using exiftool
+
+copy bayer.tif temp.dng
+
+exiftool -n^
+ -IFD0:SubfileType#=0^
+ -overwrite_original -TagsFromFile !firstFile!.dng^
+ "-all:all>all:all"^
+ -DNGVersion^
+ -DNGBackwardVersion^
+ -ColorMatrix1^
+ -ColorMatrix2^
+ "-IFD0:BlackLevelRepeatDim<SubIFD:BlackLevelRepeatDim"^
+ "-IFD0:PhotometricInterpretation<SubIFD:PhotometricInterpretation"^
+ "-IFD0:CalibrationIlluminant1<SubIFD:CalibrationIlluminant1"^
+ "-IFD0:CalibrationIlluminant2<SubIFD:CalibrationIlluminant2"^
+ -SamplesPerPixel^
+ "-IFD0:CFARepeatPatternDim<SubIFD:CFARepeatPatternDim"^
+ "-IFD0:CFAPattern2<SubIFD:CFAPattern2"^
+ -AsShotNeutral^
+ "-IFD0:ActiveArea<SubIFD:ActiveArea"^
+ "-IFD0:DefaultScale<SubIFD:DefaultScale"^
+ "-IFD0:DefaultCropOrigin<SubIFD:DefaultCropOrigin"^
+ "-IFD0:DefaultCropSize<SubIFD:DefaultCropSize"^
+ "-IFD0:OpcodeList1<SubIFD:OpcodeList1"^
+ "-IFD0:OpcodeList2<SubIFD:OpcodeList2"^
+ "-IFD0:OpcodeList3<SubIFD:OpcodeList3"^
+ !exposureTag!^
+ temp.dng >> dngmaker.log 2>>&1
+if errorlevel 1 goto err
+
+del temp.xmp
+
+set resultDNG=!firstFile!-stack!numberOfFiles!
+echo Writing composite DNG to !resultDNG!.dng using dng_validate
+dng_validate.exe -dng !resultDNG! temp.dng >> dngmaker.log 2>>&1
+
+del temp.dng
+
+echo Done
+echo.
+pause
+
+GOTO:EOF
+
+:err
+echo ERROR! Please check dngmaker.log for details
+pause
