12b13d280c2aa1927c91cdde6889a095d736c1f0
Guillermo Luijk <45771557+gluijk@users.noreply.github.com>
2021-04-26 23:57:18 +0200

    Add files via upload

--- exercise2-stacking/stacking_DNG.R
+++ exercise2-stacking/stacking_DNG.R
@@ -26,5 +26,33 @@ img=img/N
 
 
 # BUILD OUTPUT DNG
+if (max(img)<1) print(paste0("Output ETTR'ed by: +",
+                              round(-log(max(img),2),2), "EV"))
 writeTIFF(img/max(img), paste0(OUTNAME,".tif"), bits.per.sample=16,
           compression="none")
+
+
+
+# Using median instead of mean
+
+# READ RAW DATA
+
+# RAW extraction using DCRAW: dcraw -v -d -r 1 1 1 1 -t 0 -4 -T *.dng
+img=readTIFF(paste0(NAME, 18, ".tiff"), native=F, convert=F)
+img=array(0, c(nrow(img), ncol(img), N))
+for (i in 1:N) {
+    img[,,i]=readTIFF(paste0(NAME, i+17, ".tiff"), native=F, convert=F)
+}
+
+# Standard R median is VERY slow
+# Improvement: https://stackoverflow.com/questions/34771088/why-is-standard-r-median-function-so-much-slower-than-a-simple-c-alternative
+imag=apply(img, c(1,2), median)  # c(1,2) means 1st and 2nd dimensions
+
+
+
+# BUILD OUTPUT DNG
+if (max(imag)<1) print(paste0("Output ETTR'ed by: +",
+                             round(-log(max(imag),2),2), "EV"))
+writeTIFF(imag/max(imag), paste0(OUTNAME,".tif"), bits.per.sample=16,
+          compression="none")
+
