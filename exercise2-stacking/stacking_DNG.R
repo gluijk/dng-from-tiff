8f0f36f15b9dd5eb51c92ad521fb1bc2c2160d8e
Guillermo Luijk <45771557+gluijk@users.noreply.github.com>
2021-05-01 14:14:04 +0200

    Add files via upload

--- exercise2-stacking/stacking_DNG.R
+++ exercise2-stacking/stacking_DNG.R
@@ -32,7 +32,7 @@ writeTIFF(img/max(img), paste0(OUTNAME,".tif"), bits.per.sample=16,
 
 
 # Using median instead of mean
-# We improve the performance of R base mean with this C++ code:
+# We improve the performance of R median with this C++ code:
 # https://stackoverflow.com/questions/34771088/why-is-standard-r-median-function-so-much-slower-than-a-simple-c-alternative
 
 library(Rcpp)
