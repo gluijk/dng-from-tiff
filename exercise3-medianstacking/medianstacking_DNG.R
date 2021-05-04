6b17c8797b197653d4862de7e75dbffde8ffb179
Guillermo Luijk <45771557+gluijk@users.noreply.github.com>
2021-05-04 23:36:21 +0200

    Add files via upload

---
+++ exercise3-medianstacking/medianstacking_DNG.R
@@ -0,0 +1,72 @@
+# Building DNG RAW files from Bayer TIFF data
+# Exercise 3: median stacking
+# www.overfitting.net
+# https://www.overfitting.net/2021/05/apilado-por-mediana-para-eliminar.html
+
+library(tiff)
+
+
+# PARAMETERS
+N=5  # number of RAW files to merge
+NAME="raw"  # input RAW filenames
+OUTNAME="bayer"  # output RAW composite filename
+
+
+# Improve the performance of the R median function with this C++ code:
+# https://stackoverflow.com/questions/34771088/why-is-standard-r-median-function-so-much-slower-than-a-simple-c-alternative
+
+library(Rcpp)
+library(microbenchmark)
+
+cppFunction('
+double cpp_med2(Rcpp::NumericVector xx) {
+    Rcpp::NumericVector x = Rcpp::clone(xx);
+    std::size_t n = x.size() / 2;
+    std::nth_element(x.begin(), x.begin() + n, x.end());
+    
+    if (x.size() % 2) return x[n]; 
+    return (x[n] + *std::max_element(x.begin(), x.begin() + n)) / 2.;
+}
+')
+
+# Check equality and performance
+set.seed(123)
+x=rnorm(1e6)
+all.equal(median(x), cpp_med2(x))  # equality
+microbenchmark::microbenchmark(median(x), cpp_med2(x), times=200L)  # perform
+
+
+# READ RAW DATA
+
+# RAW extraction using DCRAW: dcraw -v -d -r 1 1 1 1 -t 0 -4 -T *.dng
+img=readTIFF(paste0(NAME, 1, ".tiff"), native=F, convert=F)
+img=array(0, c(nrow(img), ncol(img), N))
+for (i in 1:N) {
+    img[,,i]=readTIFF(paste0(NAME, i, ".tiff"), native=F, convert=F)
+}
+
+# MEDIAN AVERAGING 
+# median: Time difference of 14.6095 mins
+# imag=apply(img, c(1,2), median)  # c(1,2) means 1st and 2nd dimensions
+
+# cpp_med2: Time difference of 1.206512 mins (~12 times faster)
+imag=apply(img, c(1,2), cpp_med2)  # c(1,2) means 1st and 2nd dimensions
+
+
+# BUILD OUTPUT DNG
+if (max(imag)<1) print(paste0("Output ETTR'ed by: +",
+                             round(-log(max(imag),2),2), "EV"))
+writeTIFF(imag/max(imag), paste0(OUTNAME,".tif"), bits.per.sample=16,
+          compression="none")
+
+
+# Fusion map and RAW data files contributions
+mapafusion=imag*0
+for (i in 1:N) {
+    indices=which(imag==img[,,i])
+    mapafusion[indices]=i
+}
+writeTIFF((mapafusion-1)/(N-1), "mapafusion.tif", # grayscale fusion map
+          bits.per.sample=8, compression="LZW")
+for (i in 1:N) print(paste0("Contribution of ", NAME, i, ".tiff: ",
+          round(length(which(mapafusion==i))/length(mapafusion)*100,2),"%"))
