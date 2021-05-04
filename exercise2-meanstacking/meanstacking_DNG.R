c16011e6e63c19ff752a8a200733d1eca33c24c7
Guillermo Luijk <45771557+gluijk@users.noreply.github.com>
2021-05-04 23:09:19 +0200

    Add files via upload

---
+++ exercise2-meanstacking/meanstacking_DNG.R
@@ -0,0 +1,30 @@
+# Building DNG RAW files from Bayer TIFF data
+# Exercise 2: mean stacking
+# www.overfitting.net
+# https://www.overfitting.net/2021/05/apilado-por-media-simulando-iso-ultra.html
+
+library(tiff)
+
+
+# PARAMETERS
+N=16  # number of RAW files to merge
+NAME="_DSC38"  # input RAW filenames
+OUTNAME="bayer"  # output RAW composite filename
+
+
+# READ RAW DATA
+
+# RAW extraction using DCRAW: dcraw -v -d -r 1 1 1 1 -t 0 -4 -T *.dng
+img=readTIFF(paste0(NAME, 18, ".tiff"), native=F, convert=F)
+for (i in 19:(19+N-2)) {
+    img=img+readTIFF(paste0(NAME, i, ".tiff"), native=F, convert=F)
+}
+
+# MEAN AVERAGING
+img=img/N
+
+# BUILD OUTPUT DNG
+if (max(img)<1) print(paste0("Output ETTR'ed by: +",
+                              round(-log(max(img),2),2), "EV"))
+writeTIFF(img/max(img), paste0(OUTNAME,".tif"), bits.per.sample=16,
+          compression="none")
