2611f108e7ae87e35df52cc674f28067b91d50c9
Guillermo Luijk <45771557+gluijk@users.noreply.github.com>
2021-05-06 18:13:51 +0200

    Update medianstacking_DNG.R

--- exercise3-medianstacking/medianstacking_DNG.R
+++ exercise3-medianstacking/medianstacking_DNG.R
@@ -15,7 +15,6 @@ double cpp_med2(Rcpp::NumericVector xx) {
     Rcpp::NumericVector x = Rcpp::clone(xx);
     std::size_t n = x.size() / 2;
     std::nth_element(x.begin(), x.begin() + n, x.end());
-    
     if (x.size() % 2) return x[n]; 
     return (x[n] + *std::max_element(x.begin(), x.begin() + n)) / 2.;
 }
