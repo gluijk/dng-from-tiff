ba20b7c1bb09f0309a3fa26306daca96743f22aa
Guillermo Luijk <45771557+gluijk@users.noreply.github.com>
2021-05-10 23:01:37 +0200

    Add files via upload

--- dngmaker.bat
+++ dngmaker.bat
@@ -5,7 +5,7 @@ echo ========================================================================
 echo                         D N G     M A K E R                               
 echo      (by Guillermo Luijk, based on Anton Wolf's DNG Stacker script)
 echo.
-echo   This script creates a valid Bayer DNG file from a Bayer TIFF image:
+echo   This script creates a valid DNG file from RAW DNG input files:
 echo    Step 1: Use DCRAW to extract RAW data from DNG files and process it
 echo    Step 2: Use exiftool to convert a Bayer TIFF into a DNG
 echo    Step 3: Use dng_validate to make the new DNG valid and compress it
