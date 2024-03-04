81e671aec0aee88cc8bc52bbd8265a560bee5e6a
Guillermo Luijk <45771557+gluijk@users.noreply.github.com>
2024-03-04 22:00:46 +0100

    Add files via upload

--- exercise2-meanstacking/meanstacking_DNG.R
+++ exercise2-meanstacking/meanstacking_DNG.R
@@ -9,6 +9,7 @@ library(tiff)
 # PARAMETERS
 N=16  # number of RAW files to merge
 NAME="_DSC38"  # input RAW filenames
+INIT=18  # first file
 OUTNAME="bayer"  # output RAW composite filename
 
 
@@ -16,8 +17,10 @@ OUTNAME="bayer"  # output RAW composite filename
 
 # RAW extraction using DCRAW: dcraw -v -d -r 1 1 1 1 -t 0 -4 -T *.dng
 img=0
-for (i in 18:(18+N-1)) {
-    img=img+readTIFF(paste0(NAME, i, ".tiff"), native=FALSE, convert=FALSE)
+for (i in 1:N) {
+    name=paste0(NAME, i+INIT-1, ".tiff")
+    img=img+readTIFF(name, native=FALSE, convert=FALSE)
+    print(paste0(i, " / ", N, " added ", name))
 }
 
 # MEAN AVERAGING
@@ -26,5 +29,5 @@ img=img/N
 # BUILD OUTPUT DNG
 if (max(img)<1) print(paste0("Output ETTR'ed by: +",
                               round(-log(max(img),2),2), "EV"))
-writeTIFF(img/max(img), paste0(OUTNAME,".tif"), bits.per.sample=16,
+writeTIFF(img/max(img), paste0(OUTNAME, ".tif"), bits.per.sample=16,
           compression="none")
